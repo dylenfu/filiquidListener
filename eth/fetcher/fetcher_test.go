efe1f3753b16fb640a35f6ab5d5266a0d93747a1
dylenfu <dylenfu@126.com>
2024-05-28 13:31:16 +0800

    fix: go-ethereum 1.13.4 call msgarg  field not match

--- eth/fetcher/fetcher_test.go
+++ eth/fetcher/fetcher_test.go
@@ -1,13 +1,10 @@
 package fetcher
 
 import (
-	"context"
 	"os"
 	"testing"
 
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/filiquid/listener/cache"
 	"github.com/filiquid/listener/config"
 	"github.com/filiquid/listener/dao"
@@ -35,75 +32,9 @@ func TestMain(m *testing.M) {
 
 // go test -count=1 -v github.com/filiquid/listener/eth/fetcher -run TestFetchData
 func TestFetchData(t *testing.T) {
-	//testcli.client.CallContract()
-	//height := uint64(1632915)
-	// rBasic, err := testcli.caller.FetchData(nil)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// abi, err := data.DataMetaData.GetAbi()
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// for id, v := range abi.Methods {
-	// 	t.Log(id, common.Bytes2Hex(v.ID), v)
-	// }
-
-	//var out [6]common.Address
-	// list, err := testcli.Caller.GetAddresses(nil)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// t.Log(list)
-	//testcli.client.CallContract()
-	//caller.CallerABI
-	//abi.UnpackRevert()
-	// raw, err := json.Marshal(rBasic)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// t.Log(string(raw))
-
-	// testcli.Caller.MetaData.GetAbi()
-
-	// data, err := instanceABI.Pack("getValue")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// msg := ethereum.CallMsg{
-	// 	To:   &contractAddress,
-	// 	Data: data,
-	// }
-
-	// result, err := client.CallContract(context.Background(), msg, nil)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	client, err := ethclient.Dial("https://calibration.filfox.info/rpc/v1")
+	raw, err := testcli.Caller.FetchData(nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	contract := common.HexToAddress("0x8a1c17Fe673aaed1A85cA9EbB0fb494e9D43Dc6A") // governance
-	//contract := common.HexToAddress("0x86eAa93250538905ab4c9d81A3B28fa59Ae3c8B2") // dataFetcher
-	t.Log(client.BlockNumber(context.Background()))
-
-	//raw := common.Hex2Bytes("0xa39fac12") // dataFetcher.getAddresses
-	raw := common.Hex2Bytes("0x4e69d560") // governance.getStatus
-	msg := ethereum.CallMsg{
-		//From: common.HexToAddress("0x655CAA41be50c97FD75193a4A173BaBBf9A5A36C"),
-		To:   &contract,
-		Data: raw,
-		//Gas:      100000,
-		//GasPrice: big.NewInt(1000000000000000),
-	}
-
-	result, err := client.CallContract(context.Background(), msg, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// raw 0xa39fac12
-	t.Log(result)
+	t.Log(raw)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/filiquid/listener
 go 1.20
 
 require (
-	github.com/ethereum/go-ethereum v1.13.4
+	github.com/ethereum/go-ethereum v1.11.6
 	golang.org/x/crypto v0.14.0
 	golang.org/x/time v0.3.0
 	gorm.io/driver/mysql v1.5.6
@@ -11,16 +11,10 @@ require (
 )
 
 require (
-	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/StackExchange/wmi v1.2.1 // indirect
-	github.com/bits-and-blooms/bitset v1.7.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
-	github.com/consensys/bavard v0.1.13 // indirect
-	github.com/consensys/gnark-crypto v0.12.1 // indirect
-	github.com/crate-crypto/go-kzg-4844 v0.3.0 // indirect
 	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
-	github.com/ethereum/c-kzg-4844 v0.3.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-ole/go-ole v1.2.5 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
@@ -30,15 +24,12 @@ require (
 	github.com/holiman/uint256 v1.2.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
-	github.com/supranational/blst v0.3.11 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/tools v0.13.0 // indirect
-	rsc.io/tmplfunc v0.0.3 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 )
